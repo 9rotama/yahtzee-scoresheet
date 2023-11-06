75ed9bc9de6a7c5db2c09ae3a21e7b59394e3d90
9rotama <65887771+9rotama@users.noreply.github.com>
2023-11-07 03:53:36 +0900

    非プレイ中のヘッダーの作成

---
+++ src/app/(not-playing)/player/page.tsx
@@ -0,0 +1,5 @@
+import styles from "./page.module.css";
+
+export default function PlayerPage() {
+  return <div className={styles.module} />;
+}
---
+++ src/components/DefaultHeader/DefaultHeader.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { Button, Container, DropdownMenu, Flex } from "@radix-ui/themes";
+import { FiAlignJustify, FiArrowLeft } from "react-icons/fi";
+import Logo from "../Logo";
+import styles from "./DefaultHeader.module.css";
+
+function DropdownContent() {
+  return (
+    <DropdownMenu.Content>
+      <DropdownMenu.Item>プレー履歴</DropdownMenu.Item>
+    </DropdownMenu.Content>
+  );
+}
+
+export default function DefaultHeader() {
+  return (
+    <Container className={styles.container} p="2" size="2">
+      <Flex align="center" direction="row" justify="between">
+        <div className={styles.headerLeft}>
+          <Button variant="ghost" color="iris">
+            <FiArrowLeft size={20} color="#000000" />
+          </Button>
+        </div>
+        <div className={styles.headerCenter}>
+          <Logo size={40} />
+        </div>
+        <div className={styles.headerRight}>
+          <DropdownMenu.Root>
+            <DropdownMenu.Trigger>
+              <Button variant="ghost" color="iris">
+                <FiAlignJustify size={20} color="#000000" />
+              </Button>
+            </DropdownMenu.Trigger>
+            <DropdownContent />
+          </DropdownMenu.Root>
+        </div>
+      </Flex>
+    </Container>
+  );
+}
