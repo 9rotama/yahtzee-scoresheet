8339f0e7d6f349805aa7ab10a3a032180db139cd
9rotama <65887771+9rotama@users.noreply.github.com>
2023-11-10 02:28:09 +0900

    プレイヤー追加ページ作成

--- src/app/(not-playing)/player/page.tsx
+++ src/app/(not-playing)/player/page.tsx
@@ -1,5 +1,39 @@
+import PlayerList from "@/features/player/components/PlayerList";
+import { Button, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import Image from "next/image";
+import { FiArrowRight } from "react-icons/fi";
 import styles from "./page.module.css";
 
 export default function PlayerPage() {
-  return <div className={styles.module} />;
+  return (
+    <Container p="2" size="2">
+      <Flex align="center" direction="column" gap="9">
+        <Flex
+          align="center"
+          direction="column"
+          gap="5"
+          className={styles.fullWidth}
+        >
+          <Image
+            alt="プレイヤー選択"
+            src="/player_select.png"
+            width={285}
+            height={246}
+          />
+          <Heading size="5" weight="regular">
+            遊ぶ人を追加
+          </Heading>
+          <div className={styles.playerList}>
+            <PlayerList />
+          </div>
+        </Flex>
+        <Button size="4" color="jade" radius="full">
+          <Text size="2" weight="bold">
+            決定してルール選択
+          </Text>
+          <FiArrowRight />
+        </Button>
+      </Flex>
+    </Container>
+  );
 }
---
+++ src/features/player/components/PlayerList/PlayerList.tsx
@@ -0,0 +1,177 @@
+"use client";
+
+import PlayerColorDot from "@/components/PlayerColorDot";
+import { Label } from "@radix-ui/react-label";
+import {
+  Button,
+  Card,
+  Flex,
+  Popover,
+  Slider,
+  Text,
+  TextField,
+} from "@radix-ui/themes";
+import { useState } from "react";
+import { FiEdit, FiPlus, FiTrash } from "react-icons/fi";
+import styles from "./PlayerList.module.css";
+
+type PlayerSetting = {
+  name: string;
+  colorHue: number;
+};
+
+type PlayerEditPopoverProps = {
+  name: string;
+  colorHue: number;
+  editPlayer: (name: string, colorHue: number) => void;
+};
+
+function PlayerEditPopover(props: PlayerEditPopoverProps) {
+  const [value, setValue] = useState<PlayerSetting>({
+    name: props.name,
+    colorHue: props.colorHue,
+  });
+
+  return (
+    <Popover.Root>
+      <Popover.Trigger>
+        <Button variant="ghost">
+          <FiEdit color="#333333" />
+        </Button>
+      </Popover.Trigger>
+
+      <Popover.Content>
+        <Flex direction="column" gap="2">
+          <Flex direction="column">
+            <Label htmlFor="player-name">
+              <Text size="2" weight="medium">
+                Player name
+              </Text>
+            </Label>
+            <TextField.Root>
+              <TextField.Input
+                id="player-name"
+                defaultValue={props.name}
+                placeholder="input player name..."
+                onChange={(e) => {
+                  setValue({ ...value, name: e.target.value });
+                }}
+              />
+            </TextField.Root>
+          </Flex>
+          <Flex direction="column">
+            <Label htmlFor="player-color">
+              <Text size="2" weight="medium">
+                Player color
+              </Text>
+            </Label>
+            <PlayerColorDot colorHue={value.colorHue} size={20} />
+            <Slider
+              id="player-color"
+              color="jade"
+              defaultValue={[props.colorHue]}
+              onValueChange={(valueChanged) => {
+                setValue({ ...value, colorHue: valueChanged[0] });
+              }}
+              max={360}
+            />
+          </Flex>
+          <Popover.Close>
+            <Button
+              mt="2"
+              color="jade"
+              onClick={() => {
+                props.editPlayer(value.name, value.colorHue);
+              }}
+            >
+              Save
+            </Button>
+          </Popover.Close>
+        </Flex>
+      </Popover.Content>
+    </Popover.Root>
+  );
+}
+
+export default function PlayerList() {
+  const [playerList, setPlayerList] = useState<PlayerSetting[]>([]);
+  const [playerNum, setPlayerNum] = useState<number>(0);
+  const addPlayer = () => {
+    setPlayerNum(playerNum + 1);
+    setPlayerList([
+      ...playerList,
+      { name: `プレイヤー${playerNum}`, colorHue: 0 },
+    ]);
+  };
+
+  const removePlayer = (idx: number) => {
+    const playerListRemoved = playerList.slice();
+    playerListRemoved.splice(idx, 1);
+    setPlayerList(playerListRemoved);
+  };
+
+  const editPlayer = (idx: number) => {
+    return (name: string, colorHue: number) => {
+      setPlayerList(
+        playerList.map((player, i) =>
+          idx === i ? { name, colorHue } : player,
+        ),
+      );
+    };
+  };
+  return (
+    <Flex gap="2" direction="column">
+      <Flex className={styles.scroll} pl="3" pr="3" gap="2">
+        <Flex direction="column" width="100%" gap="1">
+          {playerList.length === 0 ? (
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              height="100%"
+            >
+              <Text size="4" color="gray">
+                プレイヤーが登録されていません
+              </Text>
+              <Text size="2" color="gray">
+                下のボタンよりプレイヤーを1人以上追加してください
+              </Text>
+            </Flex>
+          ) : null}
+          {playerList.map((player, i) => (
+            <Card key={i}>
+              <Flex key={player.name} align="center" gap="4" justify="between">
+                <Flex align="center" direction="row" gap="2">
+                  <PlayerColorDot colorHue={player.colorHue} size={10} />
+                  <Text size="2" className={styles.playerName}>
+                    {player.name}
+                  </Text>
+                </Flex>
+
+                <Flex align="center" gap="3">
+                  <PlayerEditPopover
+                    name={player.name}
+                    colorHue={player.colorHue}
+                    editPlayer={editPlayer(i)}
+                  />
+                  <Button
+                    variant="ghost"
+                    onClick={() => {
+                      removePlayer(i);
+                    }}
+                  >
+                    <FiTrash color="#333333" />
+                  </Button>
+                </Flex>
+              </Flex>
+            </Card>
+          ))}
+        </Flex>
+      </Flex>
+      <Button onClick={addPlayer} color="jade" variant="outline" size="2">
+        <FiPlus size="20" />
+        <Text>プレイヤーを追加</Text>
+      </Button>
+    </Flex>
+  );
+}
