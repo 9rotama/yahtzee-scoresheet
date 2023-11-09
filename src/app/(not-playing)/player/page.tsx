476f82e351096d5e8d627057728699f655a35f8f
9rotama <65887771+9rotama@users.noreply.github.com>
2023-11-10 05:03:32 +0900

    ページマウント時のアニメーション追加

--- src/app/(not-playing)/player/page.tsx
+++ src/app/(not-playing)/player/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import PlayerList from "@/features/player/components/PlayerList";
 import { Button, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { FiArrowRight } from "react-icons/fi";
@@ -7,36 +10,42 @@ import styles from "./page.module.css";
 
 export default function PlayerPage() {
   return (
-    <Container p="2" size="2">
-      <Flex align="center" direction="column" gap="9">
-        <Flex
-          align="center"
-          direction="column"
-          gap="5"
-          className={styles.fullWidth}
-        >
-          <Image
-            alt="プレイヤー選択"
-            src="/player_select.png"
-            width={285}
-            height={246}
-          />
-          <Heading size="5" weight="regular">
-            遊ぶ人を追加
-          </Heading>
-          <div className={styles.playerList}>
-            <PlayerList />
-          </div>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Container p="2" size="2">
+        <Flex align="center" direction="column" gap="9">
+          <Flex
+            align="center"
+            direction="column"
+            gap="5"
+            className={styles.fullWidth}
+          >
+            <Image
+              alt="プレイヤー選択"
+              src="/player_select.png"
+              width={285}
+              height={246}
+            />
+            <Heading size="5" weight="regular">
+              遊ぶ人を追加
+            </Heading>
+            <div className={styles.playerList}>
+              <PlayerList />
+            </div>
+          </Flex>
+          <Link href="/rule">
+            <Button size="4" color="jade" radius="full">
+              <Text size="2" weight="bold">
+                決定してルール選択
+              </Text>
+              <FiArrowRight />
+            </Button>
+          </Link>
         </Flex>
-        <Link href="/rule">
-          <Button size="4" color="jade" radius="full">
-            <Text size="2" weight="bold">
-              決定してルール選択
-            </Text>
-            <FiArrowRight />
-          </Button>
-        </Link>
-      </Flex>
-    </Container>
+      </Container>
+    </motion.div>
   );
 }
--- src/app/(not-playing)/rule/page.tsx
+++ src/app/(not-playing)/rule/page.tsx
@@ -7,71 +7,76 @@ import {
   Select,
   Text,
 } from "@radix-ui/themes";
+import { motion } from "framer-motion";
 import Image from "next/image";
 import Link from "next/link";
 import { FiArrowLeft, FiArrowRight } from "react-icons/fi";
 import styles from "./page.module.css";
 
-type Props = {};
-
 export default function RulePage() {
   return (
-    <Container p="2" size="2">
-      <Flex align="center" direction="column" gap="9">
-        <Flex
-          align="center"
-          direction="column"
-          gap="5"
-          className={styles.fullWidth}
-        >
-          <Image
-            alt="ルール選択"
-            src="/rule_select.png"
-            width={285}
-            height={246}
-          />
-          <Heading size="5" weight="regular">
-            ルールを選択
-          </Heading>
-          <Select.Root size="3" defaultValue="yathzee">
-            <Select.Trigger />
-            <Select.Content color="jade">
-              <Select.Item value="yathzee">ヤッツィー</Select.Item>
-              <Select.Item value="yams">ヤムス</Select.Item>
-            </Select.Content>
-          </Select.Root>
-        </Flex>
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Container p="2" size="2">
+        <Flex align="center" direction="column" gap="9">
+          <Flex
+            align="center"
+            direction="column"
+            gap="5"
+            className={styles.fullWidth}
+          >
+            <Image
+              alt="ルール選択"
+              src="/rule_select.png"
+              width={285}
+              height={246}
+            />
+            <Heading size="5" weight="regular">
+              ルールを選択
+            </Heading>
+            <Select.Root size="3" defaultValue="yathzee">
+              <Select.Trigger />
+              <Select.Content color="jade">
+                <Select.Item value="yathzee">ヤッツィー</Select.Item>
+                <Select.Item value="yams">ヤムス</Select.Item>
+              </Select.Content>
+            </Select.Root>
+          </Flex>
 
-        <Flex align="center" gap="1" direction="column">
-          <Link href="" className={styles.fullWidth}>
-            <Button
-              size="4"
-              color="jade"
-              radius="full"
-              className={styles.fullWidth}
-            >
-              <Text size="2" weight="bold">
-                ゲームを始める
-              </Text>
-              <FiArrowRight />
-            </Button>
-          </Link>
-          <Link href="/player">
-            <Button
-              size="4"
-              color="gray"
-              radius="full"
-              variant="outline"
-              className={styles.fullWidth}
-            >
-              <FiArrowLeft />
-              <Text size="2" weight="bold">
-                プレイヤー登録に戻る
-              </Text>
-            </Button>
-          </Link>
+          <Flex align="center" gap="1" direction="column">
+            <Link href="" className={styles.fullWidth}>
+              <Button
+                size="4"
+                color="jade"
+                radius="full"
+                className={styles.fullWidth}
+              >
+                <Text size="2" weight="bold">
+                  ゲームを始める
+                </Text>
+                <FiArrowRight />
+              </Button>
+            </Link>
+            <Link href="/player">
+              <Button
+                size="4"
+                color="gray"
+                radius="full"
+                variant="outline"
+                className={styles.fullWidth}
+              >
+                <FiArrowLeft />
+                <Text size="2" weight="bold">
+                  プレイヤー登録に戻る
+                </Text>
+              </Button>
+            </Link>
+          </Flex>
         </Flex>
-      </Flex>
-    </Container>
+      </Container>
+    </motion.div>
   );
 }
