4eba16ae077c843c000e7669a1fe2bacf8df7bf9
9rotama <65887771+9rotama@users.noreply.github.com>
2023-11-12 06:14:43 +0900

    クレジット作成

---
+++ src/app/(not-playing)/credits/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Link as RadixThemeLink,
+} from "@radix-ui/themes";
+import { motion } from "framer-motion";
+
+export default function CreditsPage() {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.3 }}
+    >
+      <Container p="2" size="2">
+        <Flex align="center" direction="column" gap="9" pt="6">
+          <Heading size="7" weight="bold">
+            クレジット
+          </Heading>
+          <Flex align="center" direction="column" gap="4">
+            <Heading size="4" weight="regular">
+              イラスト
+            </Heading>
+            <Box>
+              <RadixThemeLink href="https://dac.tsukuba.ac.jp/radd/joint-base/manga/illust/">
+                筑波大学 RADD 発達障害啓発マンガのイラストギャラリー
+              </RadixThemeLink>
+            </Box>
+          </Flex>
+          <Flex align="center" direction="column" gap="4">
+            <Heading size="4" weight="regular">
+              制作
+            </Heading>
+            <Box>
+              <RadixThemeLink href="https://9rtm.com">9rotama</RadixThemeLink>
+            </Box>
+          </Flex>
+        </Flex>
+      </Container>
+    </motion.div>
+  );
+}
--- src/components/DefaultHeader/DefaultHeader.tsx
+++ src/components/DefaultHeader/DefaultHeader.tsx
@@ -1,14 +1,18 @@
 "use client";
 
 import { Button, Container, DropdownMenu, Flex } from "@radix-ui/themes";
+import Link from "next/link";
 import { FiAlignJustify } from "react-icons/fi";
 import Logo from "../Logo";
 import styles from "./DefaultHeader.module.css";
 
 function DropdownContent() {
   return (
-    <DropdownMenu.Content>
+    <DropdownMenu.Content color="jade">
       <DropdownMenu.Item>プレー履歴</DropdownMenu.Item>
+      <Link href="/credits">
+        <DropdownMenu.Item>クレジット</DropdownMenu.Item>
+      </Link>
     </DropdownMenu.Content>
   );
 }
@@ -24,7 +28,7 @@ export default function DefaultHeader() {
         <div className={styles.headerRight}>
           <DropdownMenu.Root>
             <DropdownMenu.Trigger>
-              <Button variant="ghost" color="iris">
+              <Button variant="ghost" color="jade">
                 <FiAlignJustify size={20} color="#000000" />
               </Button>
             </DropdownMenu.Trigger>
