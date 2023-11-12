a8109cbff789a52bd9d094e37604e4b2626a074e
9rotama <65887771+9rotama@users.noreply.github.com>
2023-11-13 01:40:25 +0900

    ヘッダーのレイアウトを共通化するコンポーネント作成

--- src/components/DefaultHeader/DefaultHeader.tsx
+++ src/components/DefaultHeader/DefaultHeader.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import { Button, Container, DropdownMenu, Flex } from "@radix-ui/themes";
+import { Button, DropdownMenu } from "@radix-ui/themes";
 import Link from "next/link";
 import { FiAlignJustify } from "react-icons/fi";
-import Logo from "../Logo";
-import styles from "./DefaultHeader.module.css";
+import HeaderLayout from "../HeaderLayout";
 
 function DropdownContent() {
   return (
@@ -19,23 +18,17 @@ function DropdownContent() {
 
 export default function DefaultHeader() {
   return (
-    <Container className={styles.container} p="2" size="2">
-      <Flex align="center" direction="row" justify="between">
-        <div className={styles.headerLeft} />
-        <div className={styles.headerCenter}>
-          <Logo size={40} />
-        </div>
-        <div className={styles.headerRight}>
-          <DropdownMenu.Root>
-            <DropdownMenu.Trigger>
-              <Button variant="ghost" color="jade">
-                <FiAlignJustify size={20} color="#000000" />
-              </Button>
-            </DropdownMenu.Trigger>
-            <DropdownContent />
-          </DropdownMenu.Root>
-        </div>
-      </Flex>
-    </Container>
+    <HeaderLayout
+      right={
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <Button variant="ghost" color="jade">
+              <FiAlignJustify size={20} color="#000000" />
+            </Button>
+          </DropdownMenu.Trigger>
+          <DropdownContent />
+        </DropdownMenu.Root>
+      }
+    />
   );
 }
