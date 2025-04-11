2f63898205d678fa49c7f069ba3f95773f2d5861
9rotama <aoim99.sc@gmail.com>
2025-04-12 02:46:02 +0900

    idleレイアウトができるまで

--- app/routes.ts
+++ app/routes.ts
@@ -1,5 +1,20 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
+import {
+  type RouteConfig,
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
 
 export default [
-	layout("routes/layout.tsx", [index("routes/home.tsx")]),
+  layout("routes/layout.tsx", [
+    layout("routes/idle-layout.tsx", [
+      index("routes/home.tsx"),
+      //route("settings/players", "routes/home.tsx"),
+      //route("settings/rules", "routes/home.tsx"),
+      //route("histories", "routes/home.tsx"),
+      //route("histories/:id", "routes/home.tsx"),
+      //route("results/:id", "routes/home.tsx"),
+    ]),
+    //layout("routes/playing-layout.tsx", [route("playing", "routes/home.tsx")]),
+  ]),
 ] satisfies RouteConfig;
