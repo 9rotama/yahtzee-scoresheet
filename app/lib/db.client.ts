eeba04d9c9f6a6d1f5b84ad7a0c9999e7f5bbfc5
9rotama <aoim99.sc@gmail.com>
2025-05-03 01:02:27 +0900

    /ページからのリダイレクト

--- app/lib/db.client.ts
+++ app/lib/db.client.ts
@@ -13,7 +13,7 @@ interface DB extends DBSchema {
   };
 }
 
-const db = await openDB<DB>("yams-sheet", 1, {
+const db = openDB<DB>("yams-sheet", 1, {
   upgrade(db) {
     db.createObjectStore("currentSheet");
     initCurrentSheet();
@@ -22,15 +22,15 @@ const db = await openDB<DB>("yams-sheet", 1, {
 });
 
 export async function initCurrentSheet() {
-  const currentSheet = await db.get("currentSheet", "single");
+  const currentSheet = await (await db).get("currentSheet", "single");
   if (currentSheet) {
-    await db.put(
+    await (await db).put(
       "currentSheet",
       { players: [], rule: { name: "yahtzee" }, scores: [] },
       "single",
     );
   } else {
-    await db.add(
+    await (await db).add(
       "currentSheet",
       { players: [], rule: { name: "yahtzee" }, scores: [] },
       "single",
@@ -39,20 +39,20 @@ export async function initCurrentSheet() {
 }
 
 export async function getCurrentSheet() {
-  const currentSheet = await db.get("currentSheet", "single");
+  const currentSheet = await (await db).get("currentSheet", "single");
   if (!currentSheet) throw new Error("no current sheet found");
   return currentSheet;
 }
 
 export async function updateCurrentSheet(sheet: CurrentSheet) {
-  const currentSheet = await db.get("currentSheet", "single");
+  const currentSheet = await (await db).get("currentSheet", "single");
   if (!currentSheet) throw new Error("no current sheet found");
 
-  await db.put("currentSheet", sheet, "single");
+  await (await db).put("currentSheet", sheet, "single");
 }
 
 export async function getPreviousSheet(id: string) {
-  const sheet = await db.get("previousSheets", id);
+  const sheet = await (await db).get("previousSheets", id);
   if (!sheet) throw new Error("no previous sheet found");
   return sheet;
 }
@@ -60,6 +60,6 @@ export async function addPreviousSheet(sheet: Omit<PreviousSheet, "date">) {
   const id = nanoid();
   const date = Date.now();
 
-  await db.add("previousSheets", { ...sheet, date }, id);
+  await (await db).add("previousSheets", { ...sheet, date }, id);
   return id;
 }
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,9 +1,25 @@
-import { redirect } from "react-router";
+import { useNavigate } from "react-router";
+import { getCurrentScores } from "~/features/playing/req/index.client";
+import type { Route } from "./+types/home";
+import { use, useEffect } from "react";
 
 export function meta() {
   return [{ title: "yathzee-scoresheet" }];
 }
 
-export default function Home() {
-  redirect("/settings/players");
+export async function clientLoader() {
+  const scores = await getCurrentScores();
+  return { scores };
+}
+
+export default function Home({ loaderData }: Route.ComponentProps) {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loaderData.scores.length === 0) {
+      navigate("/settings/players");
+    } else {
+      navigate("/game");
+    }
+  }, [loaderData.scores, navigate]);
 }
